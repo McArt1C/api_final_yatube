ea685efc1044fad2e6f262c402267830d8180e39
Artem Karipov <mcartf1@yandex.ru>
2022-04-02 14:59:36 +0300

    Change after review

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import get_object_or_404
-from rest_framework import viewsets, serializers, filters
-from rest_framework.decorators import action
+from rest_framework import viewsets, filters
 from rest_framework.pagination import LimitOffsetPagination
 
 from posts.models import Post, Group
